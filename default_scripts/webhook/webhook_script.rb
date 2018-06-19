8100390dc5c5e1689a1b1b654633bd5d84f107b2
Dru Nelson <drudru@gmail.com>
2018-06-19 17:38:24 +0000

    webhook script is now able to generate a new RQ mesg

--- default_scripts/webhook/webhook_script.rb
+++ default_scripts/webhook/webhook_script.rb
@@ -2,6 +2,7 @@
 
 require 'net/http'
 require 'uri'
+require 'json'
 
 # Setup a global binding so the GC doesn't close the file
 $RQ_IO = IO.for_fd(ENV['RQ_WRITE'].to_i)
@@ -30,16 +31,29 @@ end
 
 def send_post
   # Construct form post message
+  curr_msg = JSON.parse(ENV['RQ_PARAM2'])
   mesg = {}
-  mesg['x_format'] = 'json'
-  mesg['payload'] = ENV['RQ_PARAM2']
+  keys = %w(src param1 param2 param3 param4 orig_msg_id)
+  keys.each do
+    |key|
+    next unless curr_msg.has_key?(key)
+    mesg[key] = curr_msg[key]
+  end
 
+  #mesg['_method'] = 'commit'
   uri = ENV['RQ_PARAM1']
+  parts = URI.parse(uri).path.split('/')
+  parts.pop()  # get rid of new_message
+  mesg['dest'] = parts.join('/')
+
+
+  #mesg['mesg'] = ENV['RQ_PARAM2']
+
 
   write_status('run', "Attempting post to url: #{uri}")
 
   begin
-    res = Net::HTTP.post_form(URI.parse(uri), mesg)
+    res = Net::HTTP.post_form(URI.parse(uri), {:x_format => 'json', :mesg => mesg.to_json })
   rescue
     handle_fail("Could not connect to or parse URL: #{uri}")
   end
@@ -47,6 +61,8 @@ def send_post
   if res.code.to_s =~ /2\d\d/
     write_status('done', "successfull post #{res.code.to_s}")
   else
+    STDOUT.write("#{Process.pid} - #{Time.now} - #{res.inspect}\n")
+    STDOUT.write("#{Process.pid} - #{Time.now} - #{res.body()}\n")
     handle_fail("Could not POST to URL: #{res.inspect}")
   end
 end
